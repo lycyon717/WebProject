60c73926df689e3419d36d37f363f89c704a2e5a
YUIKONNU\liang <420197665@qq.com>
2016-09-26 13:39:07 +0800

    Finish Homework 3

---
+++ out/main.js
@@ -0,0 +1,16 @@
+function strrep(num) {
+    for (var p = 0; p < num; p++) {
+        var result = "";
+        for (var i = 0; i < 10 - p; i++) {
+            result += " ";
+        }
+        for (var j = 0; j < p; j++) {
+            result += "* ";
+        }
+        console.log(result);
+    }
+}
+window.onload = function () {
+    strrep(5);
+};
+//# sourceMappingURL=main.js.map
